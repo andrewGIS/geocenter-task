6bc408f09f6acfe4f7523728d966ee38874e54fe
Mister A <andrew.tarasov1993@gmail.com>
2020-12-27 14:48:01 +0500

    Before parse

--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,20 @@
 import React, { useState, useRef, useEffect } from "react";
-
 import Map from "ol/Map";
 import View from "ol/View";
-import MultiPoint from 'ol/geom/MultiPoint'
+import MultiPoint from "ol/geom/MultiPoint";
 import TileLayer from "ol/layer/Tile";
 import { Vector as VectorLayer } from "ol/layer";
 import { OSM, Vector as VectorSource } from "ol/source";
 import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
-import { Draw, Snap } from "ol/interaction";
+import { Draw, Modify, Snap } from "ol/interaction";
+import Overlay from "ol/Overlay";
+import { toLonLat } from "ol/proj";
+import GeoJSON from "ol/format/GeoJSON";
+import Feature from 'ol/Feature'
+
+const MapToolTip = (center, text) => {
+  return <div></div>;
+};
 
 export const MapComponent = () => {
   const mapElement = useRef();
@@ -54,6 +61,57 @@ export const MapComponent = () => {
       type: "Polygon",
     });
 
+    draw.on("drawend", async (e) => {
+      let bbox = e.feature.getGeometry().getCoordinates();
+      console.log(bbox);
+      //let bbox = e.feature.getGeometry().getCoordinates()
+      console.log(bbox[0].map((point) => toLonLat(point)));
+      bbox = bbox[0].map((point) => toLonLat(point));
+      console.log(bbox)
+      // let response = await fetch (
+      //     console.log(`https://rumap.ru/?measureArea=${bbox.join(";")}`)
+      // )
+      // console.log(response.json())
+      let format = new GeoJSON({
+        // dataProjection:"EPSG:4326"
+      });
+      // let gj = new GeoJSON({
+      //   featureProjection: "EPSG:3857",
+      //   geometryName:'Polygon'
+      // })
+      let geom = e.feature.getGeometry();
+      geom.transform('EPSG:3857', 'EPSG:4326');
+      var feature = new Feature({
+        geometry: geom
+      });
+      
+      let obj = format.writeFeatures([feature]);
+      console.log(obj)
+      // console.log(JSON.stringify(obj));
+
+      // console.log(`http://gis01.rumap.ru/4898/areaStatistics?guid=93BC6341-B35E-4B34-9DFE-26796F64BBB7&geojson="${obj}"&spatialin=EPSG:3857`)
+
+      let response = await fetch(
+        `http://gis01.rumap.ru/4898/areaStatistics?guid=93BC6341-B35E-4B34-9DFE-26796F64BBB7&spatialin=EPSG:3857`,
+        {
+          method: "POST",
+          // cors:"no-cors",
+          body:obj
+        }
+      )
+
+      let data = await response.json()
+      console.log(data)
+    });
+
+    let modify = new Modify({
+      source: source,
+    });
+
+    modify.on("modifyend", (e) => {
+      console.log(e);
+    });
+
     let snap = new Snap({ source: source });
 
     const initMap = new Map({
@@ -72,22 +130,16 @@ export const MapComponent = () => {
 
     initMap.addInteraction(draw);
     initMap.addInteraction(snap);
+    initMap.addInteraction(modify);
 
     setMap(initMap);
   }, []);
-
-  const resetInteraction = () => {
-    map.interactions = []
-  }
 
   return (
     <div
       ref={mapElement}
       style={{ height: "100vh", width: "100hv" }}
       className="map-container"
-    >
-        <button onClick={resetInteraction}>Закончить рисование</button>
-    </div>
-    
+    ></div>
   );
 };
